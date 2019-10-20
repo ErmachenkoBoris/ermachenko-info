a1b04cc27a99668d27fb3181595efd03cd30efe1
Boris <cbkf12.ru@yandex.ru>
2019-10-20 17:32:59 +0300

    new design

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ let interval_id=0;
 const greetings ="Hi, my name is Boris";
 const greetingsArr = greetings.split('');
 
+const quote2 ="I told you :)";
+const quote2Arr = quote2.split('');
 let greetingAnimateEngine = (function(
     ){
         let currentLetter = 0;
@@ -14,6 +16,18 @@ let greetingAnimateEngine = (function(
             currentLetter++;
         }
     })();
+let uote2AnimateEngine = (function(
+        ){
+            let currentLetter = 0;
+            return function(){
+                if(currentLetter===quote2Arr.length) return 0;
+                let div = document.createElement('div');
+                div.className = "letter";
+                div.innerHTML = `${quote2Arr[currentLetter]}`;
+                document.getElementById("quote2").append(div);
+                currentLetter++;
+            }
+        })();
 const speedWriting = 200;
 window.onload=function() {
         if (!interval_id){
@@ -31,4 +45,7 @@ window.onblur = function() {
     interval_id = 0;
 };
 let i = 0;
+function writeQuote2(){
+    setInterval(uote2AnimateEngine, speedWriting);
+}
 
